6b76aa64061a10c1115f4acfa418c1a616fdefdc
razvanalex <razvansmadu@gmail.com>
2026-07-23 21:14:25 +0300

    fix(sunshine): disable mouse_move_enables_dpms during DPMS off to prevent mouse wakeups

--- hypr/.config/hypr/scripts/MonitorOff.sh
+++ hypr/.config/hypr/scripts/MonitorOff.sh
@@ -2,5 +2,6 @@
 
 export HYPRLAND_INSTANCE_SIGNATURE=$(ls -t $XDG_RUNTIME_DIR/hypr/ | head -n 1)
 
-# Turn off DPMS for physical display (Hyprland v0.55+ Lua API)
+# Disable mouse movement DPMS wake & turn off monitor backlight
+hyprctl eval 'hl.config({ misc = { mouse_move_enables_dpms = false } })'
 hyprctl eval 'hl.dispatch(hl.dsp.dpms({ action = "off", monitor = "HDMI-A-1" }))'
--- hypr/.config/hypr/scripts/MonitorOn.sh
+++ hypr/.config/hypr/scripts/MonitorOn.sh
@@ -2,5 +2,6 @@
 
 export HYPRLAND_INSTANCE_SIGNATURE=$(ls -t $XDG_RUNTIME_DIR/hypr/ | head -n 1)
 
-# Turn on DPMS for physical display (Hyprland v0.55+ Lua API)
+# Restore mouse movement DPMS wake & turn on monitor backlight
+hyprctl eval 'hl.config({ misc = { mouse_move_enables_dpms = true } })'
 hyprctl eval 'hl.dispatch(hl.dsp.dpms({ action = "on", monitor = "HDMI-A-1" }))'
