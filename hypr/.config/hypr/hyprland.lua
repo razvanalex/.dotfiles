c29b9bffe5af59dbcce2a8939f1aaea910b6e333
razvanalex <razvansmadu@gmail.com>
2026-08-19 22:24:28 +0300

    hypr: clear package.loaded for lua modules on config reload

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -1,6 +1,12 @@
 -- Hyprland Native Lua Entrypoint (v0.55+)
 -- ~/.config/hypr/hyprland.lua
 
+for k in pairs(package.loaded) do
+    if k:match("^lua%.") then
+        package.loaded[k] = nil
+    end
+end
+
 require("lua.variables")
 require("lua.env")
 require("lua.monitors")
